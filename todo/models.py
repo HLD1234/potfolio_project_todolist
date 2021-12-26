866da1c0e8d131380b5fc5f1cbdfc19490c528cf
Hong Long Do <honglongdo@dosoftpro.com>
2021-12-26 23:47:49 +0100

    migrated model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class ToDo(models.Model):
+    date = models.DateField((""), auto_now=False, auto_now_add=False)
+    title = models.CharField(max_length=200, blank=False, default="")
+    detail = models.CharField(max_length=200, default="")
+    complete = models.BooleanField(default=False)
